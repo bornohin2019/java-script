2ff686679cf9d9321b122d274208b6d4e6e40618
bornohin2019 <bornohin2019@gmail.com>
2024-10-20 12:57:31 +0600

    arry

---
+++ w.js
@@ -0,0 +1,30 @@
+// var x=20
+// var x=30
+// console.log(x)
+// const num1 =23;
+// const num2 =45;
+// function twoNumberSum(){
+//     const sum = 'Two numbers are'+' '+num1+' '+'and'+' '+num2+' '+'sum are'+' '+(num1+num2);
+//     return sum;
+// }
+// // call the function 
+// document.write(twoNumberSum());
+
+// object
+// const person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor: "blue"
+//   };
+// //  document.write(person());
+// console.log(person)
+// console.log(typeof(person))
+
+Array
+
+let car = ["Ferari","BMW","TATA"];
+console.log(car)
+let money = car.reverse()
+console.log(money)
+ 
